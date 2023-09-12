7c9a19b92a4e42f3124b6d4e9e507324c61bc2fe
Maarten Vermeulen <maartencvermeulen@yahoo.com>
2023-09-12 21:54:16 +0200

    Generate keys

---
+++ shellscripts/generate_keys.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# generate key
+
+# PRIVATE key
+# To generate an encrypted version, use the following command
+# expects a password
+openssl genrsa 2048 | openssl pkcs8 -topk8 -v2 des3 -inform PEM -out ../keys/rsa_key.p8
+
+# PUBLIC key
+# From the command line, generate the public key by referencing the private key. 
+# expects a password
+openssl rsa -in ../keys/rsa_key.p8 -pubout -out ../keys/rsa_key.pub
